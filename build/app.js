9b9d18bd457e32ebb9ea4ef64f9b5a5161769ca3
Maksym <maks.kmt@gmail.com>
2024-04-18 22:33:07 +0300

    kl;'jsfgh

--- build/app.js
+++ build/app.js
@@ -1,4 +1,11 @@
 var firstName = "Max";
 var text1 = "Hello ".concat(firstName, ". How are you?");
 document.getElementById("test").innerHTML = text1;
+var Color;
+(function (Color) {
+    Color["Red"] = "red";
+    Color["Green"] = "greeen";
+    Color["Blue"] = "blueew";
+})(Color || (Color = {}));
+console.log(typeof (Color));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,11 @@
 const firstName: string = "Max";
 const text1: string = `Hello ${firstName}. How are you?`;
 document.getElementById("test").innerHTML = text1;
+
+enum Color {
+    Red = "red",
+    Green = "greeen",
+    Blue = "blueew"
+}
+
+console.log(typeof(Color));
